6b2a00f21e368503e0a80e121a8780821f295b5e
Yang Xu <yxu6@ND.EDU>
2018-10-22 16:50:08 -0400

    commit change

--- sem1.r
+++ sem1.r
@@ -8,6 +8,10 @@ summary(data0)
 ## data0.noNA = na.omit(data0)
 
 colnames = sort(colnames(data0))
+data1 = data0 %>% select(AcuteStressPrior2HR, AcuteStressRelativeToDay, AcuteStressRelativeToWeek,
+                         EpisodicStressDayRelativeToWeek, EpisodicStressDayRelativeToMonth,
+                         LifetimeAvgHR, AverageCurrentHR, LifetimeAvgHRV, HrsSleep, CurrentSteps,
+                         CurrentExercizeMins, TimeOfDay, WorkDay, SurveyTime)
 write.csv(colnames, "colnames.csv")
 samples10 = data0 %>% sample_n(10) %>% select(garmin_sleep_duration, adjusted_sleep_duration,
                                               bed_time, wakeup_time, daily_sleep_debt, daily_sleep_debt_adjusted,
